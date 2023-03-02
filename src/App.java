8e8029a8c3f98be23122ec9095b49cbc1ff39d7b
Douglas Cunha <douglascunha.itau@gmail.com>
2023-03-02 19:27:31 -0300

    comit

--- src/App.java
+++ src/App.java
@@ -3,3 +3,22 @@ public class App {
         System.out.println("Hello, World!");
     }
 }
+
+class Person{
+    private String name, lastName, eyeColor, hairColor, skinColor;
+    private char gender;
+    private float weight, height;
+
+    public Person(String name, String lastName, String eyeColor, String hairColor, String skinColor, char gender,
+            float weight, float height) {
+        this.name = name;
+        this.lastName = lastName;
+        this.eyeColor = eyeColor;
+        this.hairColor = hairColor;
+        this.skinColor = skinColor;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+    }     
+    
+}
